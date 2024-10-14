0caa45c7bb5ca6982661902705599fca91b62cb9
Frances <135648147+fancypog@users.noreply.github.com>
2024-10-14 14:24:05 +0100

    Add size method by recursion

--- Week2/IntList.java
+++ Week2/IntList.java
@@ -6,6 +6,16 @@ public class IntList {
         first = f;
         rest = r;
     }
+
+    /* size method, recursion */
+    /* base case: rest == 1, size == 1 */
+    public int size() {
+        if (rest == null) {
+            return 1
+        }
+        return 1 + this.rest.size();
+    }
+
     public static void main(String[] args) {
         IntList L = new IntList(15, null);
         L = new IntList(10, L);
