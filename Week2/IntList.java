4f65973100da1091515298236b32d2f34e3db375
Frances <135648147+fancypog@users.noreply.github.com>
2024-10-14 14:35:10 +0100

    Add iterative size method

--- Week2/IntList.java
+++ Week2/IntList.java
@@ -11,15 +11,28 @@ public class IntList {
     /* base case: rest == 1, size == 1 */
     public int size() {
         if (rest == null) {
-            return 1
+            return 1;
         }
         return 1 + this.rest.size();
     }
+
+    /* iterative size method */
+    public int iterativeSize() {
+        int total = 0;
+        IntList p = this; /* a new pointer to point at the current item */
+        while (p != null) {
+            p = p.rest;
+            total ++;
+        }
+        return total;
+    }
+
 
     public static void main(String[] args) {
         IntList L = new IntList(15, null);
         L = new IntList(10, L);
         L = new IntList(5, L);
 
+        System.out.println(L.iterativeSize());
     }
 }
