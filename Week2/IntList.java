4e7e21199126c3b289e33c48341ebb302947d786
Frances <135648147+fancypog@users.noreply.github.com>
2024-10-14 14:17:26 +0100

    Add constructor

--- Week2/IntList.java
+++ Week2/IntList.java
@@ -2,18 +2,14 @@ public class IntList {
     public int first;
     public IntList rest; /* A reference/address to another IntList*/
 
-
+    public IntList(int f, IntList r) {
+        first = f;
+        rest = r;
+    }
     public static void main(String[] args) {
-        IntList L = new IntList();
-        L.first = 5;
-        L.rest = null;
+        IntList L = new IntList(15, null);
+        L = new IntList(10, L);
+        L = new IntList(5, L);
 
-        L.rest = new IntList();
-        L.rest.first = 10;
-        L.rest.rest = null;
-
-        L.rest.rest = new IntList();
-        L.rest.rest.first = 15;
-        L.rest.rest.rest = null;
     }
 }
